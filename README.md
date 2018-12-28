4aca7c5d89b70e56f2b81507cc4fb1e235811b6d
Emily Fortuna <efortuna@google.com>
2018-12-28 13:04:34 -0800

    Wrote initial library

---
+++ README.md
@@ -0,0 +1,21 @@
+# simple_future_builder
+
+A simplified FutureBuilder for the most common use case when working with Futures in Flutter.
+
+This package is hardly necessary, but it saves you a little typing.
+
+## Getting Started
+
+Usage is really simple. Just import 
+
+`import package:simple_future_builder/simple_future_builder.dart';`
+
+and then in your code, suppose you have some computation that is completing in the future, like
+`Future<String> getAsyncVersion()`. Type:
+
+    SimpleFutureBuilder<String>(
+        future: getAsyncVersion(),
+        builder: (BuildContext context, String result) => Text(text: result),
+    )
+
+Please note, if you want to deal with full error handling based on the results of the future, you will instead need to use the FutureBuilder widget.
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/widgets.dart';
+
+/// Signature for strategies that build widgets based on asynchronous interaction.
+/// 
+/// This function is only called when a future has completed.
+typedef AsyncSimpleWidgetBuilder<T> = Widget Function(BuildContext context, T futureResult);
+
+/// Extremely simple wrapper around [FutureBuilder] to reduce boilerplate for
+/// the simple case where no error handling is necessary and nothing
+/// is displayed until the future completes.
+/// 
+/// Note: if you want to deal with error handling based on what the future returns, 
+/// we recommend using [FutureBuilder].
+class SimpleFutureBuilder<T> extends StatelessWidget {
+  SimpleFutureBuilder({Key key, this.future, this.builder}) : super(key: key);
+
+  /// The asynchronous computation that this widget is waiting on before displaying a result.
+  /// 
+  /// If no future has yet completed, including in the case where [future] is null, [builder] will
+  /// will never by called.
+  final Future<T> future;
+
+  /// The build strategy currently used by this builder.
+  /// 
+  /// This function is only called once the future completes.
+  final AsyncSimpleWidgetBuilder builder;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<T>(future: this.future, 
+    builder: (BuildContext context, AsyncSnapshot<T> snapshot) {
+      return snapshot.hasData? builder(context, snapshot.data) : Container();
+    },);
+  }
+}
